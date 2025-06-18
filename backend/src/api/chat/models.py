6696b6913ac1f4178373d9a7015e0f02a0605643
Eomaxl <souravsnigdh@gmail.com>
2025-06-18 15:06:48 +0530

    [Dockerized-AI-Agent]: Implemented API to add and fetch messages

--- backend/src/api/chat/models.py
+++ backend/src/api/chat/models.py
@@ -1,4 +1,8 @@
-from sqlmodel import SQLModel, Field
+from datetime import datetime, timezone
+from sqlmodel import SQLModel, Field, DateTime
+
+def get_utc_now():
+    return datetime.now().replace(tzinfo=timezone.utc)
 
 class ChatMessagePayload(SQLModel):
     # pydantic model
@@ -10,5 +14,15 @@ class ChatMessage(SQLModel, table=True):
     # database table
     # saving, updating, getting, deleting
     id: int | None = Field(default=None, primary_key=True)
-    name: str
     message: str
+    created_at: datetime= Field(
+        default_factory=get_utc_now,
+        sa_type=DateTime(timezone=True),
+        primary_key=False,
+        nullable=False,
+    )
+
+class ChatMessageListItem(SQLModel):
+    id: int | None = Field(default=None)
+    message: str
+    created_at: datetime = Field(default=None)
--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
-from sqlmodel import Session
+from sqlmodel import Session, select
 
 from api.db import get_session
-from .models import ChatMessagePayload, ChatMessage
+from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 router = APIRouter()
 
 # /api/chats/
@@ -10,8 +10,17 @@ router = APIRouter()
 def chat_health():
     return {"status":"ok"}
 
+# /api/chats/recent
+# curl http://localhost:8080/api/chats/recent/
+@router.get("/recent/", response_model=List[ChatMessageListItem])
+def chat_list_messages(session: Session = Depends(get_session)):
+    query = select(ChatMessage)
+    results = session.exec(query).fetchall()[:10]
+    return results
+
 
 # HTTP Post -> payload = {"message":"Hello World"} -> {"message":"hello world", "id": 1}
+# curl -X POST -d '{"message":"Hello World"}' -H "Content-Type: application/json" http://localhost:8080/api/chats
 @router.post("/", response_model=ChatMessage)
 def chat_create_message(
     payload:ChatMessagePayload,
