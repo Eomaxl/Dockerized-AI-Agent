7e42f1adab11409a4868de2b9d0edcc2c9460433
Eomaxl <souravsnigdh@gmail.com>
2025-06-21 12:45:07 +0530

    [Dockerized-AI-Agent]: Added api routes to AI services

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from api.db import get_session
+from api.ai.services import generate_email_message
+from api.ai.schemas import EmailMessageSchema
 from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 router = APIRouter()
 
@@ -22,7 +24,7 @@ def chat_list_messages(session: Session = Depends(get_session)):
 
 # HTTP Post -> payload = {"message":"Hello World"} -> {"message":"hello world", "id": 1}
 # curl -X POST -d '{"message":"Hello World"}' -H "Content-Type: application/json" http://localhost:8080/api/chats
-@router.post("/", response_model=ChatMessage)
+@router.post("/", response_model=EmailMessageSchema)
 def chat_create_message(
     payload:ChatMessagePayload,
     session: Session = Depends(get_session)
@@ -32,6 +34,7 @@ def chat_create_message(
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    session.refresh(obj) # ensure id/primary key added to the obj instance
+    # session.refresh(obj) # ensure id/primary key added to the obj instance
     # ready to store in the database
+    response = generate_email_message(payload.message)
     return obj
